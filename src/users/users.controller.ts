092c0e9db7e50fb27f538efbd74da1933bfa7ff4
Manik <manik@pixelconception.com>
2024-03-08 23:11:13 +0530

    updating code

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,29 @@
-import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+  Body,
+  Req,
+} from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('users')
 export class UsersController {
-  @Get()
-  public getUsers() {
+  @Get('/:id/:optional?')
+  public getUsers(@Param() params: any, @Query() query: any) {
+    console.log(params);
+    console.log(query);
     return 'You sent a get request to users endpoint';
   }
 
   @Post()
-  public createUsers() {
+  public createUsers(@Body() request: any) {
+    console.log(request);
     return 'You sent a post request to users endpoint';
   }
 }

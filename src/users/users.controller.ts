1d342b21ea74ca6d26386ca9e3e1bb8cd3bb4e52
Manik <manik@pixelconception.com>
2024-03-08 17:19:45 +0530

    Added Users Controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  public getUsers() {
+    return 'You sent a get request to users endpoint';
+  }
+
+  @Post()
+  public createUsers() {
+    return 'You sent a post request to users endpoint';
+  }
+}

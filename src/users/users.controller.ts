e41ad4d7bb373e28b6fda719bef2f8ce0d647032
Manik <manik@pixelconception.com>
2024-03-08 23:25:54 +0530

    UPdated Code

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,20 +8,28 @@ import {
   Put,
   Query,
   Body,
+  Headers,
+  Ip,
 } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get('/:id/:optional?')
-  public getUsers(@Param() params: any, @Query() query: any) {
-    console.log(params);
-    console.log(query);
+  public getUsers(@Param('id') id: any, @Query('limit') limit: any) {
+    console.log(id);
+    console.log(limit);
     return 'You sent a get request to users endpoint';
   }
 
   @Post()
-  public createUsers(@Body() request: any) {
+  public createUsers(
+    @Body() request: any,
+    @Headers() headers: any,
+    @Ip() ip: any,
+  ) {
     console.log(request);
+    console.log(headers);
+    console.log(ip);
     return 'You sent a post request to users endpoint';
   }
 }

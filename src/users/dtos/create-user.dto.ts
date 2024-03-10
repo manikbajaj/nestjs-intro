811075330b9f84e37119fa5b8ae005be0e49d002
Manik <manik@pixelconception.com>
2024-03-10 12:49:35 +0530

    adding code

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,30 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(96)
+  firstName: string;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(3)
+  @MaxLength(96)
+  lastName?: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,14 +10,31 @@ import {
   Body,
   Headers,
   Ip,
+  ParseIntPipe,
+  DefaultValuePipe,
 } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-  @Get('/:id/:optional?')
-  public getUsers(@Param('id') id: any, @Query('limit') limit: any) {
-    console.log(id);
+  /**
+   * Final Endpoint - /users/id?limit=10&page=1
+   * Parama id - optional, convert to integer, cannot have a default value
+   * Query limit - integer, default 10
+   * Query page - integer, default value 1
+   * ==> USE CASES
+   * /users/ -> return all users with default pagination
+   * /users/1223 -> returns one user whos id is 1234
+   * /users?limit=10&page=2 -> return page 2 with limt of pagination 10
+   */
+
+  @Get('/:id?')
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
     console.log(limit);
+    console.log(page);
     return 'You sent a get request to users endpoint';
   }
 

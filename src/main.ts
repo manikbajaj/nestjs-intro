d9d11e2ecfbe40a216dc1560793e8795f0f6e5d9
Manik <manik@pixelconception.com>
2024-03-11 11:55:07 +0530

    updating code

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /*
+   * Use validation pipes globally
+   */
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
   await app.listen(3000);
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -42,7 +42,7 @@ export class UsersController {
 
   @Post()
   public createUsers(@Body() createUserDto: CreateUserDto) {
-    console.log(createUserDto);
+    console.log(createUserDto instanceof CreateUserDto);
     return 'You sent a post request to users endpoint';
   }
 }

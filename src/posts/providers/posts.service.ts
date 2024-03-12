677470b0ddb505faf56ecf136a9a551e9325df87
Manik <manik@pixelconception.com>
2024-03-12 17:10:44 +0530

    update

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -3,6 +3,18 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class PostsService {
   public findAll(userId: string) {
-    console.log(userId);
+    // Users Service
+    // Find A User
+
+    return [
+      {
+        title: 'Test Tile',
+        content: 'Test Content',
+      },
+      {
+        title: 'Test Tile 2',
+        content: 'Test Content 2',
+      },
+    ];
   }
 }

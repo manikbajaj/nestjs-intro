bccb448f7d6735a8bbe3a589e1760458d184b2f8
Manik <manik@pixelconception.com>
2024-05-11 12:32:56 +0530

    Final Code for NSJS128

--- src/meta-options/meta-options.controller.ts
+++ src/meta-options/meta-options.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { CreatePostMetaOptionsDto } from './dtos/create-post-meta-options.dto';
+import { MetaOptionsService } from './meta-options.service';
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('meta-options')
-export class MetaOptionsController {}
+export class MetaOptionsController {
+  constructor(
+    /**
+     * Inject MetaOptionsService
+     * */
+    private readonly MetaOptionsService: MetaOptionsService,
+  ) {}
+
+  @Post()
+  public async create(
+    @Body() createPostMetaOptionsDto: CreatePostMetaOptionsDto,
+  ) {
+    return this.MetaOptionsService.create(createPostMetaOptionsDto);
+  }
+}
--- src/meta-options/meta-options.module.ts
+++ src/meta-options/meta-options.module.ts
@@ -2,9 +2,11 @@ import { MetaOption } from './meta-option.entity';
 import { MetaOptionsController } from './meta-options.controller';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MetaOptionsService } from './meta-options.service';
 
 @Module({
   controllers: [MetaOptionsController],
   imports: [TypeOrmModule.forFeature([MetaOption])],
+  providers: [MetaOptionsService],
 })
 export class MetaOptionsModule {}

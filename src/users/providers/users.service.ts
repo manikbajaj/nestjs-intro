4e48a2a907abd537c41922236e2b9983542a2f03
Manik <manik@pixelconception.com>
2024-05-19 14:40:41 +0530

    Final Code for NSJS184

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -3,7 +3,7 @@ import {
   BadRequestException,
   Inject,
   Injectable,
-  InternalServerErrorException,
+  RequestTimeoutException,
   forwardRef,
 } from '@nestjs/common';
 import { User } from '../user.entity';
@@ -36,14 +36,32 @@ export class UsersService {
   ) {}
 
   public async createUser(createUserDto: CreateUserDto) {
-    // Check if user with email exists
-    const existingUser = await this.usersRepository.findOne({
-      where: { email: createUserDto.email },
-    });
+    let existingUser = undefined;
+
+    try {
+      // Check if user with email exists
+      existingUser = await this.usersRepository.findOne({
+        where: { email: createUserDto.email },
+      });
+    } catch (error) {
+      // Might want to save these errors with more information in a log file or database
+      // You don't need to send this sensitive information to user
+      throw new RequestTimeoutException(
+        'Unable to process your request at the moment please try later',
+        {
+          description: 'Error connecting to database',
+        },
+      );
+    }
 
     /**
      * Handle exceptions if user exists later
      * */
+    if (existingUser) {
+      throw new BadRequestException(
+        'The user already exists, please check your email',
+      );
+    }
 
     // Try to create a new user
     // - Handle Exceptions Later

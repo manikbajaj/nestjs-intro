8c0d627b2fb6ed84e18b1b47ab74945e22401354
Manik <manik@pixelconception.com>
2024-05-26 14:54:10 +0530

    Final Code for NSJS208

--- src/users/providers/users-create-many.provider.ts
+++ src/users/providers/users-create-many.provider.ts
@@ -1,7 +1,12 @@
+import {
+  ConflictException,
+  Injectable,
+  RequestTimeoutException,
+} from '@nestjs/common';
+
 import { CreateManyUsersDto } from '../dtos/create-many-users.dto';
 import { CreateUserDto } from '../dtos/create-user.dto';
 import { DataSource } from 'typeorm';
-import { Injectable } from '@nestjs/common';
 import { User } from '../user.entity';
 
 @Injectable()
@@ -19,11 +24,14 @@ export class UsersCreateManyProvider {
     // Create Query Runner Instance
     const queryRunner = this.dataSource.createQueryRunner();
 
-    // Connect the query ryunner to the datasource
-    await queryRunner.connect();
-
-    // Start the transaction
-    await queryRunner.startTransaction();
+    try {
+      // Connect the query ryunner to the datasource
+      await queryRunner.connect();
+      // Start the transaction
+      await queryRunner.startTransaction();
+    } catch (error) {
+      throw new RequestTimeoutException('Could not connect to the database');
+    }
 
     try {
       for (let user of createManyUsersDto.users) {
@@ -35,9 +43,18 @@ export class UsersCreateManyProvider {
     } catch (error) {
       // since we have errors lets rollback the changes we made
       await queryRunner.rollbackTransaction();
+      throw new ConflictException('Could not complete the transaction', {
+        description: String(error),
+      });
     } finally {
-      // you need to release a queryRunner which was manually instantiated
-      await queryRunner.release();
+      try {
+        // you need to release a queryRunner which was manually instantiated
+        await queryRunner.release();
+      } catch (error) {
+        throw new RequestTimeoutException(
+          'Could not release the query runner connection',
+        );
+      }
     }
 
     return newUsers;

b2b918cddf7294bfe425de462d81d543604ede48
Manik <manik@pixelconception.com>
2024-05-26 11:53:55 +0530

    Update posts.service.ts

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,14 +1,12 @@
-import { PatchPostDto } from './../dtos/patch-post.dto';
-import { TagsService } from './../../tags/providers/tags.service';
-import { CreatePostDto } from '../dtos/create-post.dto';
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/providers/users.service';
+import { CreatePostDto } from '../dtos/create-post.dto';
 import { Repository } from 'typeorm';
 import { Post } from '../post.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MetaOption } from 'src/meta-options/meta-option.entity';
-import { User } from 'src/users/user.entity';
-import { waitForDebugger } from 'inspector';
+import { TagsService } from 'src/tags/providers/tags.service';
+import { PatchPostDto } from '../dtos/patch-post.dto';
 
 @Injectable()
 export class PostsService {
@@ -17,46 +15,47 @@ export class PostsService {
      * Injecting Users Service
      */
     private readonly usersService: UsersService,
-
     /**
-     * Injecting postsRepository
+     * Inject postsRepository
      */
     @InjectRepository(Post)
     private readonly postsRepository: Repository<Post>,
-
     /**
-     * Injecting Tags service
+     * inject metaOptionsRepository
+     */
+    @InjectRepository(MetaOption)
+    private readonly metaOptionsRepository: Repository<MetaOption>,
+    /**
+     * Inject TagsService
      */
     private readonly tagsService: TagsService,
   ) {}
 
   /**
-   * Method to create a new post
+   * Creating new posts
    */
-  public async create(createPostDto: CreatePostDto) {
+  public async create(@Body() createPostDto: CreatePostDto) {
+    // Find author from database based on authorId
     let author = await this.usersService.findOneById(createPostDto.authorId);
-
+    // Find tags
     let tags = await this.tagsService.findMultipleTags(createPostDto.tags);
 
-    // Create the post
+    // Create post
     let post = this.postsRepository.create({
       ...createPostDto,
       author: author,
       tags: tags,
     });
 
+    // return the post
     return await this.postsRepository.save(post);
   }
 
-  /**
-   * Method to find all posts
-   */
   public async findAll(userId: string) {
-    // find all posts
     let posts = await this.postsRepository.find({
       relations: {
         metaOptions: true,
-        author: true,
+        //author: true,
         // tags: true,
       },
     });
@@ -64,31 +63,36 @@ export class PostsService {
     return posts;
   }
 
-  /**
-   * Method to delete a post from the database
-   */
-  public async delete(id: number) {
-    // Find the post from the database
-    await this.postsRepository.delete(id);
-
-    return { deleted: true, id };
-  }
-
-  /**
-   * Method to Update a post
-   */
   public async update(patchPostDto: PatchPostDto) {
-    // Find new tags
+    // Find the Tags
     let tags = await this.tagsService.findMultipleTags(patchPostDto.tags);
 
-    // Update the post
+    // Find the Post
     let post = await this.postsRepository.findOneBy({
       id: patchPostDto.id,
     });
 
-    // Update the tags
+    // Update the properties
+    post.title = patchPostDto.title ?? post.title;
+    post.content = patchPostDto.content ?? post.content;
+    post.status = patchPostDto.status ?? post.status;
+    post.postType = patchPostDto.postType ?? post.postType;
+    post.slug = patchPostDto.slug ?? post.slug;
+    post.featuredImageUrl =
+      patchPostDto.featuredImageUrl ?? post.featuredImageUrl;
+    post.publishOn = patchPostDto.publishOn ?? post.publishOn;
+
+    // Assign the new tags
     post.tags = tags;
 
+    // Save the post and return
     return await this.postsRepository.save(post);
+  }
+
+  public async delete(id: number) {
+    // Deleting the post
+    await this.postsRepository.delete(id);
+    // confirmation
+    return { deleted: true, id };
   }
 }

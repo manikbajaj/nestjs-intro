edcdc32b59879f42eff84926e9975a4792ffcb30
Manik <manik@pixelconception.com>
2024-06-03 14:52:44 +0530

    Final code for NSJS240

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,13 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module, forwardRef } from '@nestjs/common';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './providers/auth.service';
 import { BcryptProvider } from './providers/bcrypt.provider';
 import { HashingProvider } from './providers/hashing.provider';
-import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 import { SignInProvider } from './providers/sign-in.provider';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   controllers: [AuthController],
@@ -17,7 +19,16 @@ import { SignInProvider } from './providers/sign-in.provider';
     },
     SignInProvider,
   ],
-  imports: [forwardRef(() => UsersModule)],
+  imports: [
+    forwardRef(() => UsersModule),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+      }),
+    }),
+  ],
   exports: [AuthService, HashingProvider],
 })
 export class AuthModule {}

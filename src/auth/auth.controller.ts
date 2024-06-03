6af0d700bd99c7e75dc371b4d33a1fa4891ef9a8
Manik <manik@pixelconception.com>
2024-06-03 14:37:09 +0530

    Final Code For NSJS238

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 
 import { AuthService } from './providers/auth.service';
 import { SignInDto } from './dtos/signin.dto';
@@ -13,6 +13,7 @@ export class AuthController {
   ) {}
 
   @Post()
+  @HttpCode(HttpStatus.OK)
   public signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
   }

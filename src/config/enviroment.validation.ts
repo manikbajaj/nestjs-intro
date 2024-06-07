b7a333895f18dc658c88afa118143b9c41aaf0d8
Manik <manik@pixelconception.com>
2024-06-07 11:06:23 +0530

    Final Code for NSJS257

--- src/config/enviroment.validation.ts
+++ src/config/enviroment.validation.ts
@@ -10,4 +10,8 @@ export default Joi.object({
   DATABASE_NAME: Joi.string().required(),
   DATABASE_USER: Joi.string().required(),
   PROFILE_API_KEY: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
+  JWT_TOKEN_AUDIENCE: Joi.required(),
+  JWT_TOKEN_ISSUER: Joi.string().required(),
+  JWT_ACCESS_TOKEN_TTL: Joi.number().required(),
 });

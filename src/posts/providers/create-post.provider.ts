bcdb036cafc8eb2d82ce501d79ed2e319752455f
Manik <manik@pixelconception.com>
2024-06-09 16:39:06 +0530

    Updating create-post

--- src/posts/providers/create-post.provider.ts
+++ src/posts/providers/create-post.provider.ts
@@ -1,30 +1,33 @@
 import {
   BadRequestException,
+  Body,
   ConflictException,
   Injectable,
-  RequestTimeoutException,
 } from '@nestjs/common';
 import { CreatePostDto } from '../dtos/create-post.dto';
-import { TagsService } from 'src/tags/providers/tags.service';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Post } from '../post.entity';
-import { Repository } from 'typeorm';
-import { ActiveUserData } from 'src/auth/interfaces/active-user-data.interface';
 import { UsersService } from 'src/users/providers/users.service';
+import { Repository } from 'typeorm';
+import { Post } from '../post.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TagsService } from 'src/tags/providers/tags.service';
+import { ActiveUserData } from 'src/auth/interfaces/active-user-data.interface';
 
 @Injectable()
 export class CreatePostProvider {
   constructor(
-    /**
-     * Inject TagsService
+    /*
+     * Injecting Users Service
      */
-    private readonly tagsService: TagsService,
-
+    private readonly usersService: UsersService,
     /**
      * Inject postsRepository
      */
     @InjectRepository(Post)
     private readonly postsRepository: Repository<Post>,
+    /**
+     * Inject TagsService
+     */
+    private readonly tagsService: TagsService,
   ) {}
 
   public async create(createPostDto: CreatePostDto, user: ActiveUserData) {
@@ -32,16 +35,16 @@ export class CreatePostProvider {
     let tags = undefined;
 
     try {
+      // Find author from database based on authorId
+      author = await this.usersService.findOneById(user.sub);
       // Find tags
       tags = await this.tagsService.findMultipleTags(createPostDto.tags);
     } catch (error) {
-      throw new RequestTimeoutException(error, {
-        description: 'Could not fetch tags',
-      });
+      throw new ConflictException(error);
     }
 
     if (createPostDto.tags.length !== tags.length) {
-      throw new BadRequestException('Please check tag Ids');
+      throw new BadRequestException('Please check your tag Ids');
     }
 
     // Create post

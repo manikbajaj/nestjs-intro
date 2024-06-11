d431ec476ea2bf7a95e4edfd65076d145e443850
Manik <manik@pixelconception.com>
2024-06-11 22:38:19 +0530

    Final Code for NSJS282

--- src/config/enviroment.validation.ts
+++ src/config/enviroment.validation.ts
@@ -14,4 +14,5 @@ export default Joi.object({
   JWT_TOKEN_AUDIENCE: Joi.required(),
   JWT_TOKEN_ISSUER: Joi.string().required(),
   JWT_ACCESS_TOKEN_TTL: Joi.number().required(),
+  JWT_REFRESH_TOKEN_TTL: Joi.number().required(),
 });

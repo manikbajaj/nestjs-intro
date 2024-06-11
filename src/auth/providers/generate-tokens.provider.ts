e692d4ebc026fab53adf8c6842f841dadbe8f2ec
Manik <manik@pixelconception.com>
2024-06-11 22:53:38 +0530

    Final Code For NSJS284

--- src/auth/providers/generate-tokens.provider.ts
+++ src/auth/providers/generate-tokens.provider.ts
@@ -2,6 +2,8 @@ import { Inject, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import jwtConfig from '../config/jwt.config';
 import { ConfigType } from '@nestjs/config';
+import { User } from 'src/users/user.entity';
+import { ActiveUserData } from '../interfaces/active-user-data.interface';
 
 @Injectable()
 export class GenerateTokensProvider {
@@ -31,5 +33,20 @@ export class GenerateTokensProvider {
         expiresIn,
       },
     );
+  }
+
+  public async generateTokens(user: User) {
+    const [accessToken, refreshToken] = await Promise.all([
+      this.signToken<Partial<ActiveUserData>>(
+        user.id,
+        this.jwtConfiguration.accessTokenTtl,
+        { email: user.email },
+      ),
+      this.signToken(user.id, this.jwtConfiguration.refreshTokenTtl),
+    ]);
+    return {
+      accessToken,
+      refreshToken,
+    };
   }
 }

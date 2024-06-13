4611c1db371919f8cc9239c953def958a372148b
Manik <manik@pixelconception.com>
2024-06-13 12:52:57 +0530

    Final code for NSJS293

--- src/auth/social/providers/google-authentication.service.ts
+++ src/auth/social/providers/google-authentication.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit, forwardRef } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import jwtConfig from 'src/auth/config/jwt.config';
+import { OAuth2Client } from 'google-auth-library';
 
 @Injectable()
-export class GoogleAuthenticationService {}
+export class GoogleAuthenticationService implements OnModuleInit {
+  private oauthClient: OAuth2Client;
+
+  constructor(
+    /**
+     * Inject jwtConfiguration
+     */
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
+
+  onModuleInit() {
+    const clientId = this.jwtConfiguration.googleClientId;
+    const clientSecret = this.jwtConfiguration.googleClientSecret;
+    this.oauthClient = new OAuth2Client(clientId, clientSecret);
+  }
+}

95f0deb1c4cbb5600475500b8f7bfa02a2157b68
Manik <manik@pixelconception.com>
2024-06-19 12:03:04 +0530

    Final Code for NSJS365

--- src/users/providers/users.service.spec.ts
+++ src/users/providers/users.service.spec.ts
@@ -1,13 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { CreateGoogleUserProvider } from './create-google-user.provider';
+import { CreateUserProvider } from './create-user.provider';
+import { DataSource } from 'typeorm';
+import { FindOneByGoogleIdProvider } from './find-one-by-google-id.provider';
+import { FindOneUserByEmailProvider } from './find-one-user-by-email.provider';
+import { User } from '../user.entity';
+import { UsersCreateManyProvider } from './users-create-many.provider';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: DataSource, useValue: {} },
+        { provide: getRepositoryToken(User), useValue: {} },
+        { provide: CreateUserProvider, useValue: {} },
+        { provide: UsersCreateManyProvider, useValue: {} },
+        { provide: FindOneUserByEmailProvider, useValue: {} },
+        { provide: FindOneByGoogleIdProvider, useValue: {} },
+        { provide: CreateGoogleUserProvider, useValue: {} },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);

7be6fa6cfa71c46f8868cc9477f95ea657201086
Manik <manik@pixelconception.com>
2024-06-19 15:28:01 +0530

    Final code for NSJS368

--- src/users/providers/create-user.provider.spec.ts
+++ src/users/providers/create-user.provider.spec.ts
@@ -1,27 +1,35 @@
+import { DataSource, Repository } from 'typeorm';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { CreateUserProvider } from './create-user.provider';
-import { DataSource } from 'typeorm';
 import { HashingProvider } from 'src/auth/providers/hashing.provider';
 import { MailService } from 'src/mail/providers/mail.service';
 import { User } from '../user.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
 
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+const createMockRepository = <T = any>(): MockRepository<T> => ({
+  findOne: jest.fn(),
+  create: jest.fn(),
+});
+
 describe('CreateUserProvider', () => {
   let provider: CreateUserProvider;
+  let usersRepository: MockRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         CreateUserProvider,
         { provide: DataSource, useValue: {} },
-        { provide: getRepositoryToken(User), useValue: {} },
+        { provide: getRepositoryToken(User), useValue: createMockRepository() },
         { provide: HashingProvider, useValue: {} },
         { provide: MailService, useValue: {} },
       ],
     }).compile();
 
     provider = module.get<CreateUserProvider>(CreateUserProvider);
+    usersRepository = module.get(getRepositoryToken(User));
   });
 
   it('Should Be Defined', () => {

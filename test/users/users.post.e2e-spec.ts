7d936eb3c86b66bae33b399f0fc3778cfc5eca40
Manik <manik@pixelconception.com>
2024-06-21 12:39:59 +0530

    Final Code For NSJS384

---
+++ test/users/users.post.e2e-spec.ts
@@ -0,0 +1,29 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { AppModule } from 'src/app.module';
+import { INestApplication } from '@nestjs/common';
+
+describe('[Users] @Post Endpoints', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it.todo('/users - Endpoint is public');
+  it.todo('/users - firstName is mandatory');
+  it.todo('/users - lastName is mandatory');
+  it.todo('/users - email is mandatory');
+  it.todo('/users - Valid request successfully creates user');
+  it.todo('/users - password is not returned in response');
+  it.todo('/users - googleId is not returned in response');
+});

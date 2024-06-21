77f8455ab76c4d341ab14821ac625ebc5d94d753
Manik <manik@pixelconception.com>
2024-06-21 13:19:17 +0530

    Final Code fo NSJS385

--- test/users/users.post.e2e-spec.ts
+++ test/users/users.post.e2e-spec.ts
@@ -1,21 +1,31 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { AppModule } from 'src/app.module';
 import { INestApplication } from '@nestjs/common';
+import { appCreate } from 'src/app.create';
+import { dropDatabase } from 'test/helpers/drop-database.helper';
 
 describe('[Users] @Post Endpoints', () => {
   let app: INestApplication;
+  let config: ConfigService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, ConfigModule],
+      providers: [ConfigService],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    // Add all app middleware
+    appCreate(app);
+    config = app.get<ConfigService>(ConfigService);
+
     await app.init();
   });
 
   afterEach(async () => {
+    await dropDatabase(config);
     await app.close();
   });
 
